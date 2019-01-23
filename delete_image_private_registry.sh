6b89ec2b75930c58dfc538326fec5b8f80b516ae
Victor Chen <victorchen276@gmail.com>
2019-01-23 21:02:44 +1300

    Create delete_image_private_registry.sh

---
+++ delete_image_private_registry.sh
@@ -0,0 +1,11 @@
+registry='192.168.1.5:5000'
+name='image name'
+curl -v -sSL -X DELETE "http://${registry}/v2/${name}/manifests/$(
+    curl -sSL -I \
+        -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+        "http://${registry}/v2/${name}/manifests/$(
+            curl -sSL "http://${registry}/v2/${name}/tags/list" | jq -r '.tags[0]'
+        )" \
+    | awk '$1 == "Docker-Content-Digest:" { print $2 }' \
+    | tr -d $'\r' \
+)"
